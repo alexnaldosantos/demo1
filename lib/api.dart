a1ffe5c5723cc6b30a821aa1b3e5c4c86b2f960e
Alexnaldo Santos <3rd.alexnaldo.santos@amedigital.com>
2023-02-09 10:15:40 -0300

    http

---
+++ lib/api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:demo1/post.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Post>> getPosts() async {
+  
+  const url = 'https://jsonplaceholder.typicode.com/posts';
+
+  final client = http.Client();
+  final response = await client.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    List<dynamic> jsonPosts = jsonDecode(response.body);
+    List<Post> posts = jsonPosts.map((post) => Post.fromJson(post)).toList();
+    return posts;
+  } else {
+    throw Exception('Failed to load posts');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:demo1/api.dart';
+import 'package:demo1/post.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,17 +50,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Post> posts = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _fetchData() async {
+    final newPosts = await getPosts();
+    setState(() => posts = newPosts);
   }
 
   @override
@@ -75,41 +71,32 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: posts.isNotEmpty
+          ? _lista()
+          : _button(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _button() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextButton(onPressed: _fetchData, child: const Text("Get data"))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _lista() {
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(posts[index].title),
+          subtitle: Text(posts[index].body),
+        );
+      },
     );
   }
 }
